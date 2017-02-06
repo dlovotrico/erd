64a5fb44deb1847e491816760a43a122f5162915
Diego <diego@nucleoid.net>
2017-02-06 06:43:46 -0300

    Initial commit after retaking the project, MVC reinterpreted and the previous Views system eliminated, now the previously theme part takes the function of Views. Also most of the components of HTML boilerplate eliminated.

--- core/controllers/frontpage.php
+++ core/controllers/frontpage.php
@@ -1,21 +1,1 @@
-<?php
-
-/**
- * <b>Controlador de índices</b>.
- * 
- * 
- * @author      Diego Lovotrico <diego@nucleoid.net>
- * 
- * @version     Version 0.1
- * @since       Version 0.1
- * @category    Controladores
- * 
- */
-
-class Frontpage extends Controller {
-    function __construct() {
-        // Llama al constructor del controlador madre, el cual
-        // incluye todos los elementos afines a todos los controladores.
-        parent::__construct();
-    }
-}
+<?php/** * <b>Index controller</b>. *  *  * @author      Diego Lovotrico <diego@nucleoid.net> *  * @version     Version 0.1 * @since       Version 0.1 * @category    Controllers *  */class Frontpage extends Controller {    function __construct() {        // Calls for the parent constructor, which includes all the elements common        // to all the controllers.        parent::__construct();    }}
--- core/models/sanitizer.php
+++ core/models/sanitizer.php
@@ -1,70 +1,1 @@
-<?php
-/** 
- * <b>Higienización inteligente de cadenas</b>
- *
- * Cada método o clase que requiera una cadena higienizada utilizará
- * Sanitizer() para su limpieza. Esta clase brinda distintos niveles
- * de higiene para distintos tipos de inputs y outputs. 
- * 
- * 
- * @author      Diego Lovotrico <diego@nucleoid.net>
- * 
- * @version     Version 0.1
- * @since       Version 0.1
- */
-
-
-class Sanitizer {
-   /**
-    * <b>Higie de cadenas</b>
-    * 
-    * Este método es el "motor" de la clase. 
-    * 
-    * 
-    * @access   private
-    * 
-    * @param    string  $str    Cadena a limpiar
-    * 
-    * @param    integer $level  Nivel de limpieza, mientras más alto sea el nivel
-    *                           más se limpiará la cadena de caracteres especiales 
-    *                           y demás.
-    *                               - 1: output para un usuario
-    *                               - 2: database querries
-    *                               - 3: URLs, URIs, etc.
-    * 
-    * @return string
-    */ 
-    private function sanitizeString($str, $level = 3) 
-    {
-        if($level == 3)
-        {
-            // Eliminar chars no alfanuméricos
-            $str = preg_replace('#[^-a-zA-Z0-9_ ]#', '', $str);  
-            // Formatea guiones para la URL
-            $str = preg_replace('#[-_ ]+#', '-', $str);
-        }
-
-        if($level == 3 OR 2)
-        {
-            // Escapar caracteres especiales
-            $str = htmlspecialchars($str);
-            // eliminar espacios en blanco 
-            $str = trim($str);
-        }
-        return $str;
-   }
-
-
-   // ------------------> METODOS PUBLICOS
-    public function fromUrl($url) {
-        return $this->sanitizeString($url,3);
-    }
-    
-    public function fromQuery($query) {
-        return $this->sanitizeString($query,2);        
-    }
-
-    public function toOutput($output) {
-        return $this->sanitizeString($output,1);     
-    }
-}
+<?php/**  * <h1>Input string sanitization engine</h1> * * Each method or class which requires a sanitized string can use Sanitizer(). * <strong>This class uses different levels of sanitization for differente types * of inputs and outputs.</strong> *  *  *  * ---- *  *  *  * @author      Diego Lovotrico <diego@nucleoid.net> *  * @version     Version 0.1 * @since       Version 0.1 */class Sanitizer {     ####--------------------------------------------------------------------[PRIVATE METHODS]##        /**    * <h1>String sanitization</h1>    * - This method is the engine of the class.    *     *     * ----    *     *     *     * @access   private    *     * @param    string  $str    Sting to sanitize    *     * @param    integer $level  Cleaning level, the higher the highest to level.    *                               - 1: User output    *                               - 2: DB queries    *                               - 3: URLs, URIs, etc.    *     * @return string    */            private function sanitizeString($str, $level = 3)     {        if($level == 3)        {            // Alphanumeric characters cleaning.            $str = preg_replace('#[^-a-zA-Z0-9_ ]#', '', $str);              // Dashes formatting for the URL.            $str = preg_replace('#[-_ ]+#', '-', $str);        }        if($level == 3 OR 2)        {            // Escape special chars.            $str = htmlspecialchars($str);            // Remove whte spaces.             $str = trim($str);        }        return $str;   }               ####--------------------------------------------------------------------[PUBLIC METHODS]##    public function fromUrl($url) {        return $this->sanitizeString($url,3);    }                public function fromQuery($query) {        return $this->sanitizeString($query,2);            }            public function toOutput($output) {        return $this->sanitizeString($output,1);         }}
