34fb6e9f8797e1c2207a2f1d273d02ac938cfb06
Lovotrico <dlov@nucleoid.net>
2019-11-15 21:46:53 -0300

    Mindmap documentation.

---
+++ CMS.mm
@@ -0,0 +1,15 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#333333" COLOR="#ffffff" CREATED="1547599392135" ID="ID_1831348473" MODIFIED="1547600253415" TEXT="CMS">
+<font BOLD="true" NAME="SansSerif" SIZE="24"/>
+<node BACKGROUND_COLOR="#ffff00" CREATED="1547600152200" ID="ID_1392132160" MODIFIED="1547600285255" POSITION="right" STYLE="bubble" TEXT="Current problems">
+<edge WIDTH="1"/>
+<font BOLD="true" NAME="Calibri" SIZE="18"/>
+</node>
+<node CREATED="1547600310184" ID="ID_159466233" MODIFIED="1547600315498" POSITION="left" TEXT="Tasks">
+<node CREATED="1547605221988" ID="ID_297907290" MODIFIED="1547605238957" TEXT="Add a redirector for internal code">
+<node CREATED="1547605446385" ID="ID_793154440" MODIFIED="1547605452147" TEXT="See how vichan did it"/>
+</node>
+</node>
+</node>
+</map>
